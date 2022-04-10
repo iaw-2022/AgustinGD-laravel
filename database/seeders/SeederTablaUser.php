2d176663127180a2af19d4a008b7be24a64a050d
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-04-10 14:39:18 -0300

    seeder para usuario administrador

---
+++ database/seeders/SeederTablaUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SeederTablaUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'administrador',
+            'email' => 'administrador@yahoo.org',
+            'password' => bcrypt('iaw2022administrador')
+        ]);
+    }
+}
