32676fd48af4efcd3bea2e10f37bb510bd7fbf34
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-04-13 20:06:15 -0300

    home no se usa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Producto;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $productos = Producto::all();
+        return view('producto.index')->with('productos', $productos);
     }
 }
