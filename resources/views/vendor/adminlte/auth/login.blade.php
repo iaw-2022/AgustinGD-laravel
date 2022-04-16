c803fd581d35e7b7e0915c8ccc9967a6b717f1ca
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-04-16 12:04:11 -0300

    fix para el css

--- resources/views/vendor/adminlte/auth/login.blade.php
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('adminlte::auth.auth-page', ['auth_type' => 'login'])
 
 @section('adminlte_css_pre')
-    <link rel="stylesheet" href="{{ asset('vendor/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css">
 @stop
 
 @php( $login_url = View::getSection('login_url') ?? config('adminlte.login_url', 'login') )
