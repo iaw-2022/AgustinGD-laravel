ea80c2898c3a112e3282c564286730005b381373
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-04-24 11:50:28 -0300

    Codigo mas limpio para los roles

--- database/migrations/2022_04_14_191850_relacion_usuario_rol.php
+++ database/migrations/2022_04_14_191850_relacion_usuario_rol.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Rol;
 
 return new class extends Migration
 {
@@ -14,7 +15,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('rol_id')->default(4);
+            $table->unsignedBigInteger('rol_id')->default(Rol::IS_USER);
             $table->foreign('rol_id')->references('id')->on('rols')->onUpdate('cascade')->onDelete('restrict');
         });
     }
