bcdff01067e4cb649fba033149cbe984ed43dd29
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-04-24 12:20:38 -0300

    Correccion al nombre de la columna

--- database/migrations/2022_04_24_144231_create_clientes_table.php
+++ database/migrations/2022_04_24_144231_create_clientes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
             $table->string('email')->unique();
             $table->timestamps();
         });
