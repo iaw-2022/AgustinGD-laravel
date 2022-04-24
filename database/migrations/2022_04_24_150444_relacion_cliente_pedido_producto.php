bc983b729b40a7c69219507a61541383a4bec4c3
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-04-24 12:12:24 -0300

    Agregando modelo para Pedidos

---
+++ database/migrations/2022_04_24_150444_relacion_cliente_pedido_producto.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pedidos', function (Blueprint $table) {
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onUpdate('cascade')->onDelete('restrict');
+            $table->foreign('producto_id')->references('id')->on('productos')->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pedidos', function (Blueprint $table) {
+            $table->dropForeign('pedidos_cliente_id_foreign');
+            $table->dropForeign('pedidos_producto_id_foreign');
+        });
+    }
+};
