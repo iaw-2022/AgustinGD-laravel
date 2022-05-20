508005a81aa17867004c8879ea4fd11f7aec36d8
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-20 19:59:24 -0300

    Agregando imagenes para las categorias

--- database/migrations/2022_04_11_023348_create_categorias_table.php
+++ database/migrations/2022_04_11_023348_create_categorias_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->string('nombre', 50);
-            $table->string('descripcion', 255);
+            $table->string('descripcion', 255);            
+            $table->string('imagen_dir', 255);
             $table->timestamps();
         });
     }
--- database/seeders/SeederTablaCategoria.php
+++ database/seeders/SeederTablaCategoria.php
@@ -18,38 +18,45 @@ class SeederTablaCategoria extends Seeder
         $this->crear(
             'Carnes',
             'Si es musculo es valido.',
+            'https://res.cloudinary.com/dtkj9tvgw/image/upload/v1651665859/app-granja/leche_vzix8k.png',
         );
 
         $this->crear(
             'Lacteos',
             'Si es de una ubre mejor.',
+            'https://res.cloudinary.com/dtkj9tvgw/image/upload/v1651665859/app-granja/leche_vzix8k.png',
         );
 
         $this->crear(
             'Vegetales',
             'Los arboles son vegetales.',
+            'https://res.cloudinary.com/dtkj9tvgw/image/upload/v1651665859/app-granja/leche_vzix8k.png',
         );
 
         $this->crear(
             'Frutas',
             'Fruto comestible de ciertas plantas y árboles, en especial cuando tiene mucha agua y es de sabor dulce.',
+            'https://res.cloudinary.com/dtkj9tvgw/image/upload/v1651665859/app-granja/leche_vzix8k.png',
         );
 
         $this->crear(
             'Organos',
             'Mejor adentro que afuera.',
+            'https://res.cloudinary.com/dtkj9tvgw/image/upload/v1651665859/app-granja/leche_vzix8k.png',
         );
 
         $this->crear(
             'Nueces',
             'Categoria favorita de Alvin y las Ardillas.',
+            'https://res.cloudinary.com/dtkj9tvgw/image/upload/v1651665859/app-granja/leche_vzix8k.png',
         );
     }
 
-    private function crear($nombre, $descripcion){
+    private function crear($nombre, $descripcion, $imagen_dir){
         DB::table('categorias')->insert([
             'nombre' => $nombre,
             'descripcion' => $descripcion,
+            'imagen_dir' => $imagen_dir,
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
     }
